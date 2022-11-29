f4a137bbcd3dffbd350d9f010c4d59204a0a8511
loknath pradhan <loknath11a@gmail.com>
2022-11-29 15:32:29 +0530

    added post and delete request

--- server/src/routes/userContact.js
+++ server/src/routes/userContact.js
@@ -2,38 +2,77 @@ const bodyParser = require("body-parser");
 const express = require("express");
 const Contact = require("../models/contact");
 const router = express.Router();
+// const bodyParser = require("body-parser");
+const { default: mongoose } = require("mongoose");
 
-
+router.use(bodyParser.urlencoded({ extended: false }));
 router.use(bodyParser.json());
 
-router.post("/post", async (req,res) => {
+router.post("/post", async (req, res) => {
     try {
-        
-        // console.log(req.body)
-        const {name,designation,company,industry,email,phone,country}=req.body;
-        const contact = await Contact.create({
-            name: name,
-            designation: designation,
-            company: company,
-            industry: industry,
-            email: email,
-            phone: phone,
-            country: country,
-            // user: req.user
+
+        let contactArray = req.body;
+        contactArray.forEach(element => {
+
+            const { name, designation, company, industry, email, phone, country } = element;
+
+            let contact =  Contact.create({
+                name: name,
+                designation: designation,
+                company: company,
+                industry: industry,
+                email: email,
+                phone: phone,
+                country: country,
+                // user: req.user
+            });
         });
+
         res.json({
             status: "Sucess",
-            contact
-    
+            // contact
+
         });
 
     } catch (error) {
         res.status(500).json({
             status: "Failed",
-            message : error.message
+            message: error.message
+        })
+    }
+});
+
+
+router.delete("/delete", async (req, res) => {
+
+    try {
+
+        let deletArray = req.body;
+        if (deletArray.length > 0) {
+            deletArray = deletArray.map((d) => mongoose.Types.ObjectId(d));
+            const contact = await Contact.deleteMany({ _id: { $in: deletArray } });
+            res.json({
+                status: 'success',
+                contact
+            })
+        }
+        else {
+            const contact = await Contact.deleteMany();
+            res.json({
+                status: 'success',
+                contact
+            })
+        }
+
+    } catch (error) {
+        res.status(500).json({
+            status: 'Failed',
+            message: error.message
         })
     }
 })
+
+
 
 
 module.exports = router
