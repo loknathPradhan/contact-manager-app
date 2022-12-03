f9c57e4fa6b1ba8702304be9aaee3cc054df676e
loknath pradhan <loknath11a@gmail.com>
2022-12-03 09:53:21 +0530

    fixed bug with get request in import

--- client/contact_manager/src/App.js
+++ client/contact_manager/src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import ImportUI from "./import/ImportUI";
+import { AuthProvider } from "../src/useauth/Useauth";
+import Signup from "./signup/Signup";
+import Login from "./login/Login";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<Navigate replace to="/signup" />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route
+          path="/login"
+          element={
+            <AuthProvider>
+              <Login />
+            </AuthProvider>
+          }
+        />
+        <Route
+          path="/contact"
+          element={
+            <AuthProvider>
+              <ImportUI />
+            </AuthProvider>
+          }
+        />
+        <Route path="*" element={<Signup />} />
+      </Routes>
+    </Router>
   );
 }
 
